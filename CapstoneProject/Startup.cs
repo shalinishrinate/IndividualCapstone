15d084b45a1e776e0a7d4966bebd9755e30e4f29
Shalini Shrinate <you@example.com>
2020-01-28 08:22:59 -0600

    Created Person role.

--- CapstoneProject/Startup.cs
+++ CapstoneProject/Startup.cs
@@ -1,4 +1,7 @@
-﻿using Microsoft.Owin;
+﻿using CapstoneProject.Models;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using Microsoft.Owin;
 using Owin;
 
 [assembly: OwinStartupAttribute(typeof(CapstoneProject.Startup))]
@@ -9,6 +12,26 @@ namespace CapstoneProject
         public void Configuration(IAppBuilder app)
         {
             ConfigureAuth(app);
+            createRolesandUsers();
+        }
+
+
+        // In this method we will create default User roles and Admin user for login    
+        private void createRolesandUsers()
+        {
+            ApplicationDbContext context = new ApplicationDbContext();
+
+            var roleManager = new RoleManager<IdentityRole>(new RoleStore<IdentityRole>(context));
+            
+            // creating Creating Person role     
+            if (!roleManager.RoleExists("Person"))
+            {
+                var role = new Microsoft.AspNet.Identity.EntityFramework.IdentityRole();
+                role.Name = "Person";
+                roleManager.Create(role);
+
+            }
+
         }
     }
 }
